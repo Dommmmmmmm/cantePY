fa705810bdc69b94b932995063a5e464c6cdd32e
Dommmmmmmm <74880419+Dommmmmmmm@users.noreply.github.com>
2021-11-29 00:42:31 +0100

    Add files via upload

---
+++ fattoriale.cante.py
@@ -0,0 +1,17 @@
+n=int(input('inserire il numero: '))
+f=1
+
+for i in range(1,n+1)
+    f*=in
+
+    print(n) 
+    n = int('inserire il numero: ')
+
+    def fattoriale (n)
+      if n==0
+         return 1
+    else:
+            return n*fattoriale(n-1)
+
+    print ('il fattoriale é',  fattoriale(n)) 
+    
